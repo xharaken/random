9dd2674be7b8ea587c94051f53721983175e4a78
Kentaro Hara <haraken@google.com>
2023-05-22 19:12:16 +0900

    added calculator_expected

--- calculator_expected.py
+++ calculator_expected.py
@@ -1,19 +1,30 @@
 #! /usr/bin/python3
+# Run this program with python3 (not python2)
 
 def read_number(line, index):
     number = 0
-    while index < len(line) and line[index].isdigit():
-        number = number * 10 + int(line[index])
+    flag = 0
+    keta = 1
+    while index < len(line) and (line[index].isdigit() or line[index] == '.'):
+        if line[index] == '.':
+            flag = 1
+        else:
+            number = number * 10 + int(line[index])
+            if flag == 1:
+                keta *= 0.1
         index += 1
-    if index < len(line) and line[index] == '.':
-        index += 1
-        keta = 0.1
-        while index < len(line) and line[index].isdigit():
-            number += int(line[index]) * keta
-            keta /= 10
-            index += 1
-    token = {'type': 'NUMBER', 'number': number}
+    token = {'type': 'NUMBER', 'number': float(number * keta)}
     return token, index
+
+
+def read_plus(line, index):
+    token = {'type': 'PLUS'}
+    return token, index + 1
+
+
+def read_minus(line, index):
+    token = {'type': 'MINUS'}
+    return token, index + 1
 
 
 def read_multiply(line, index):
@@ -26,13 +37,13 @@ def read_divide(line, index):
     return token, index + 1
 
 
-def read_plus(line, index):
-    token = {'type': 'PLUS'}
+def read_left(line, index):
+    token = {'type': 'LEFT'}
     return token, index + 1
 
 
-def read_minus(line, index):
-    token = {'type': 'MINUS'}
+def read_right(line, index):
+    token = {'type': 'RIGHT'}
     return token, index + 1
 
 
@@ -50,6 +61,10 @@ def tokenize(line):
             (token, index) = read_multiply(line, index)
         elif line[index] == '/':
             (token, index) = read_divide(line, index)
+        elif line[index] == '(':
+            (token, index) = read_left(line, index)
+        elif line[index] == ')':
+            (token, index) = read_right(line, index)
         else:
             print('Invalid character found: ' + line[index])
             exit(1)
@@ -57,8 +72,25 @@ def tokenize(line):
     return tokens
 
 
-def evaluate(tokens):
-    return evaluate_plus_and_minus(evaluate_multiply_and_divide(tokens))
+def evaluate_brackets(tokens):
+    index = 0
+    bracket_left_index = 0
+    while index < len(tokens):
+        # Find the right most ')'
+        if tokens[index]['type'] == 'RIGHT':
+            for i in reversed(range(0, index)):
+                # Find the corresponding '('
+                if tokens[i]['type'] == 'LEFT':
+                    left_index = i
+                    break
+
+            # Evaluate an expression inside the bracket
+            value = evaluate(tokens[left_index + 1 : index])
+            tokens[index] = {'type': 'NUMBER', 'number': value}
+            del tokens[left_index : index]
+            index = left_index
+        index += 1
+    return tokens
 
 
 def evaluate_multiply_and_divide(tokens):
@@ -94,6 +126,12 @@ def evaluate_plus_and_minus(tokens):
     return answer
 
 
+def evaluate(tokens):
+    tokens = evaluate_brackets(tokens)
+    tokens = evaluate_multiply_and_divide(tokens)
+    return evaluate_plus_and_minus(tokens)
+
+
 def test(line):
     tokens = tokenize(line)
     actual_answer = evaluate(tokens)
@@ -127,6 +165,25 @@ def run_test():
     test("1.1*2.2")
     test("1.1/2.2")
     test("1.1*2.2-3.3*4.4-5.5/6.6+7.7/8.8")
+    test("(1)")
+    test("(1+2)")
+    test("((1))")
+    test("((1.1))")
+    test("(1+2)*3")
+    test("(1+2)/3")
+    test("1*(2+3)")
+    test("1/(2+3)")
+    test("(1+2)*(3+4)")
+    test("(1+2)/(3+4)")
+    test("1-(2-3)")
+    test("1-(2-(3-4))")
+    test("1-(2-(3-(4-5)))")
+    test("1-(2-(3-(4-5)-6))")
+    test("1-(2-(3-(4-5)-6))")
+    test("1-(2-(3-(4-5)-6)-7)")
+    test("1-(2-(3-(4-5)-6)-7)-8")
+    test("(1+2)*3-4/(5-(6-7*8+9/(10-11)*12))")
+    test("(1.1+2.2)*3.3-4.4/(5.5-(6.5-7.7*8.8+9.9/(10.0-11.1)*12.2))")
     print("==== Test finished! ====\n")
 
 run_test()
