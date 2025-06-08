ce6aecde2165cbee5aab708f4075e50ef3415233
Kentaro Hara <haraken@google.com>
2025-06-08 18:03:43 +0900

    minor edit

--- wikipedia_expected.py
+++ wikipedia_expected.py
@@ -15,6 +15,12 @@ class Wikipedia:
         # from the page whose ID is 1234.
         self.links = {}
 
+        # A set of reverse page links.
+        # For example, self.links[1234] returns an array of page IDs that have
+        # links to the page whose ID is 1234.
+        # This is initialized in build_reverse_graph().
+        self.reverse_links = {}
+
         # Read the pages file into self.titles.
         with open(pages_file) as file:
             for line in file:
@@ -72,8 +78,8 @@ class Wikipedia:
 
     # Homework #1:
     # Find the shortest path.
-    # 'start': The title of the start page.
-    # 'goal': The title of the goal page.
+    # 'start': A title of the start page.
+    # 'goal': A title of the goal page.
     def find_shortest_path(self, start, goal):
         start_id = self.title_to_id(start)
         goal_id = self.title_to_id(goal)
@@ -190,10 +196,10 @@ class Wikipedia:
         print()
 
 
-    # Optional homework:
+    # Homework #3 (optional):
     # Search the longest path with heuristics.
-    # 'start': The title of the start page.
-    # 'goal': The title of the goal page.
+    # 'start': A title of the start page.
+    # 'goal': A title of the goal page.
     def find_longest_path(self, start, goal):
         start_id = self.title_to_id(start)
         goal_id = self.title_to_id(goal)
@@ -204,131 +210,200 @@ class Wikipedia:
             print("The page %s was not found\n" % goal)
             return
 
-        # Build a reverse graph.
-        reverse_links = self.build_reverse_links()
+        # Run DFS from the start page to the goal page and find an initial
+        # path.
+        path = self.dfs_with_heuristics(start_id, goal_id, {})
+        self.assert_path(path, start, goal)
+        print("Initial path: %d" % (len(path) - 1))
 
-        # Run BFS from the goal page using the reverse graph.
-        # As a result, distance(X -> goal page) on the original graph is stored
-        # in distances[X].
+        # Repeat until interrupted.
+        while True:
+            # We try to extend the path incrementally with the following
+            # algorithm.
+            #
+            # 1. Randomly select one node in the current path. Imagine that
+            # an 'index'-th node in the path is selected.
+            #
+            # The current path is:
+            #   start == path[0] -> path[1] -> ... ->
+            #   path[index] -> path[index+1] -> ... -> path[-1] == goal
+            #
+            # 2. We set visited flags on all nodes in the path except
+            # path[index] and path[index+1], and run DFS from path[index] to
+            # path[index+1]. If we find a longer path from path[index] to
+            # path[index+1], replace path[index] -> path[index+1] with the
+            # found one.
+
+            assert(len(path) >= 2)
+            # Randomly select one node in the path.
+            index = random.randint(0, len(path) - 2)
+
+            # Before running DFS, we need to set visited flags on all nodes
+            # in the path except path[index] and path[index+1].
+            visited = {}
+            for i, node in enumerate(path):
+                if i != index and i != index + 1:
+                    visited[node] = True
+
+            # Run DFS from path[index] to path[index+1].
+            new_path = self.dfs_with_heuristics(
+                path[index], path[index + 1], visited)
+
+            if len(new_path) >= 3:
+                # If a longer path is found, update the path.
+                path = (path[:index] + new_path + path[index + 2:])
+                self.assert_path(path, start, goal)
+                print("Updated path: %d" % (len(path) - 1))
+            else:
+                print("No update")
+
+
+    # Helper function for Homework #3:
+    # Please use this function to check if the found path is well formed.
+    # 'path': An array of page IDs that stores the found path.
+    #     path[0] is the start page. path[-1] is the goal page.
+    #     path[0] -> path[1] -> ... -> path[-1] is the path from the start
+    #     page to the goal page.
+    # 'start': A title of the start page.
+    # 'goal': A title of the goal page.
+    def assert_path(self, path, start, goal):
+        assert(start != goal)
+        assert(len(path) >= 2)
+        assert(self.titles[path[0]] == start)
+        assert(self.titles[path[-1]] == goal)
+        for i in range(len(path) - 1):
+            assert(path[i + 1] in self.links[path[i]])
+
+
+    # DFS with heuristics.
+    # 'start_id': An ID of the start page.
+    # 'goal_id': An ID of the goal page.
+    # 'visited': Visited flags.
+    #
+    # This function returns a found path.
+    def dfs_with_heuristics(self, start_id, goal_id, visited):
+        current = start_id
+        previous = {}
+        previous[current] = None
+        stack = collections.deque([current])
+        while stack:
+            current = stack.pop()
+            # For DFS to find a longer path, it is important to set a visited
+            # flag when the node is popped from the stack, not when the node is
+            # pushed to the stack.
+            #
+            # For example, imagine that A has links to B and C. When DFS
+            # visits A, it pushes B and C to the stack. DFS pops C from the
+            # stack and visits C. If a visited flag is set when the node is
+            # pushed to the stack, DFS will never find A -> C -> ... -> B
+            # because B's visited flag is already set. DFS only finds A -> B.
+            visited[current] = True
+
+            if current == goal_id:
+                path = []
+                while current:
+                    path.append(current)
+                    current = previous[current]
+                path.reverse()
+                return path
+
+            # Randomize the order to visit child nodes. Otherwise, DFS always
+            # finds the same path and we cannot extend the initial path at all.
+            children = self.links[current].copy()
+            random.shuffle(children)
+
+            for child in children:
+                if not child in visited:
+                    # Do not set a visited flag here. See above comment.
+                    previous[child] = current
+                    stack.append(child)
+        return []
+
+
+    # DFS with advanced heuristics. This finds a longer path more efficiently.
+    # 'start_id': An ID of the start page.
+    # 'goal_id': An ID of the goal page.
+    # 'visited': Visited flags.
+    #
+    # This function returns a found path.
+    def dfs_with_advanced_heuristics(self, start_id, goal_id, visited):
+        # Build a reverse graph.
+        reverse_links = self.build_reverse_graph()
+
+        # Calculate distance(X -> goal page) on the original graph for all
+        # node X's and store it to distances[X]. We can do this by running
+        # BFS from the goal page to all nodes using the reverse graph.
         current = goal_id
         distances = {}
         distances[current] = 0
         queue = collections.deque([(current, distances[current])])
         while queue:
             (current, distance) = queue.popleft()
-            for child in reverse_links[current]:
+            for child in self.reverse_links[current]:
                 if not child in distances:
                     distances[child] = distance + 1
                     queue.append((child, distances[child]))
-        print("BFS from the goal page finished.")
 
-        max_path = 0
-        # Repeat until interrupted.
-        while True:
-            # Run DFS from the start page. To find a longer path to the goal
-            # page, twist the order to visit child nodes. Instead of visiting
-            # the child nodes in the order stored in self.links, visit child
-            # nodes that are more distant to the goal page first. That way,
-            # DFS is more likely to find a longer path to the goal page.
-            current = start_id
-            previous = {}
-            previous[current] = None
-            visited = {}
+        # Run DFS from the start page. To find a longer path to the goal
+        # page, twist the order to visit child nodes. Instead of visiting
+        # the child nodes in the order stored in self.links, visit child
+        # nodes that are more distant to the goal page first. By doing this,
+        # DFS is more likely to find a longer path to the goal page.
+        current = start_id
+        previous = {}
+        previous[current] = None
+        stack = collections.deque([current])
+        while stack:
+            current = stack.pop()
+            # For DFS to find a longer path, the visited flag needs to be set
+            # when the node is popped from the stack, not when the node is
+            # pushed to the stack.
             visited[current] = True
-            stack = collections.deque([current])
-            while stack:
-                current = stack.pop()
-                # For DFS to find a longer path, it is important to set a
-                # visited flag when the node has been visited, not when the
-                # node is pushed to the stack. If a visited flag is set when
-                # the node X is pushed to the stack, DFS gives up searching a
-                # longer path to X when the node X is pushed to the stack.
-                #
-                # For example, imagine that A has links to B and C. When DFS
-                # visits A, it pushes B and C to the stack. DFS pops C from the
-                # stack and visits C. If a visited flag is set when the node is
-                # pushed to the stack, DFS will never find A -> C -> ... -> B
-                # because B's visited flag is already set. DFS only finds
-                # A -> B.
-                visited[current] = True
 
-                # The path from the start page to the goal page is found.
-                if current == goal_id:
-                    routes = []
-                    while current:
-                        routes.append(self.titles[current])
-                        current = previous[current]
-                    routes.reverse()
-                    path = len(routes) - 1
-                    print("Path found: %d" % path)
-                    # Update the max path.
-                    if max_path < path:
-                        max_path = path
-                        # print(" -> ".join(routes), "\n")
-                        print("Max path found: %d" % path)
-                    break
+            if current == goal_id:
+                path = []
+                while current:
+                    path.append(current)
+                    current = previous[current]
+                path.reverse()
+                return path
 
-                # Twist the order to visit child nodes so that DFS visits
-                # child nodes that are more distant to the goal page first.
-                children = self.shuffle_nodes(self.links[current], distances)
+            # Twist the order to visit child nodes so that DFS visits child
+            # nodes that are more distant to the goal page first. For this
+            # purpose, we sort the child nodes in the order of the distances
+            # to the goal page.
+            children = []
+            for node in self.links[current]:
+                # If 'node' is not included in 'distances', there is no path
+                # from the 'node' to the goal page. We do not need to visit
+                # the 'node'.
+                if node in distances:
+                    children.append(node)
+            # Sort the child nodes by distances to the goal page.
+            children = sorted(children, key=lambda x:distances[x])
 
-                for child in children:
-                    # This condition should not be 'if not child in previous'.
-                    # For DFS to find a longer path, the visited flag needs to
-                    # be set when the node has been visited, not when the node
-                    # is pushed to the stack.
-                    if not child in visited:
-                        previous[child] = current
-                        stack.append(child)
+            for child in children:
+                if not child in visited:
+                    previous[child] = current
+                    stack.append(child)
+        return []
 
 
     # Helper function:
-    # Bulid a graph with reverse links.
-    # self.links is a set of all page links of the original graph.
+    # Build a graph with reverse links.
     # If the original graph has a link from a page X to a page Y,
     # the reverse graph has a link from a page Y to a page X.
-    # This method returns a set of all page links of the reverse graph.
-    def build_reverse_links(self):
-        reverse_links = {}
+    # The reverse links are stored in self.reverse_links.
+    def build_reverse_graph(self):
+        if self.reverse_links:
+            return
         for id in self.titles.keys():
-            reverse_links[id] = []
+            self.reverse_links[id] = []
         for src in self.links.keys():
             for dst in self.links[src]:
                 # If the original graph has a link from 'src' to 'dst',
                 # the reverse graph has a link from 'dst' to 'src'.
-                reverse_links[dst].append(src)
-        return reverse_links
-
-
-    # Helper function:
-    # Twist the order of visiting child nodes.
-    # 'nodes': A list of child nodes.
-    # 'distances': distances[X] stores distance(X -> goal page).
-    #
-    # This method sorts 'nodes' in the order of the distances to the goal page.
-    # nodes[0] is the least distant node to the goal page, and nodes[-1] is the
-    # most distant node to the goal page.
-    # To add some randomness, swap a few items in the sorted nodes.
-    def shuffle_nodes(self, nodes, distances):
-        result = []
-        for node in nodes:
-            # if 'node' is not in 'distances', there is no path from 'node'
-            # to the goal page.
-            if node in distances:
-                result.append((node, distances[node]))
-
-        # Sort the child nodes in the order of the distances to the goal page.
-        result = sorted(result, key=lambda x:x[1])
-
-        # To add randomness, swap a few items when there are many child nodes.
-        if len(result) >= 20:
-            for _ in range(len(result) // 20):
-                # Avoid swapping the first item because the first item may be
-                # the goal node. DFS should visit it last.
-                index1 = random.randint(1, len(result) - 1)
-                index2 = random.randint(1, len(result) - 1)
-                result[index1], result[index2] = result[index2], result[index1]
-        return [item[0] for item in result]
+                self.reverse_links[dst].append(src)
 
 
 if __name__ == "__main__":
@@ -345,5 +420,5 @@ if __name__ == "__main__":
     wikipedia.find_shortest_path("渋谷", "パレートの法則")
     # Homework #2
     wikipedia.find_most_popular_pages()
-    # Optional homework
+    # Homework #3 (optional)
     wikipedia.find_longest_path("渋谷", "池袋")
